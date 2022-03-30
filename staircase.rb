3bafaa385f9bf847760c018b6117dfee7a6624fd
greg <gregorybwest@gmail.com>
2022-03-29 23:09:30 -0700

    solves hacker-rank 'staircase' challenge

--- staircase.rb
+++ staircase.rb
@@ -1,8 +1,22 @@
-def staircase(height)
-  i = 1
-  while i <= height
-    puts "#" * i 
-    i += 1
+# def staircase(height)
+#   i = 1
+#   while i <= height
+#     puts "#" * i 
+#     i += 1
+#   end
+# end
+
+# staircase(5)
+
+# n = height
+def staircase(n)
+  spaces = n - 1
+  blocks = 1
+  while spaces >= 0 && blocks <= n
+    print " " * spaces
+    puts "#" * blocks
+    blocks += 1
+    spaces -= 1
   end
 end
 
