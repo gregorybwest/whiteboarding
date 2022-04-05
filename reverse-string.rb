db3beb8436545d8cde93cda0d103c980d36b095f
greg <gregorybwest@gmail.com>
2022-04-05 13:47:24 -0700

    refactor of reverse-string

--- reverse-string.rb
+++ reverse-string.rb
@@ -9,13 +9,24 @@
 # Loop through each element of the array of characters starting at the end of the array, pushing each element into the reversed string variable. 
 # Return the reversed string
 
+# def reverse_string(string)
+#   reversed_string = []
+#   index = string.length - 1
+#   string_array = string.split("")
+#   while index >= 0
+#     reversed_string.push(string_array[index])
+#     index -= 1
+#   end
+#   reversed_string.join
+# end
+
+# p reverse_string("how are you")
+
 def reverse_string(string)
   reversed_string = []
-  index = string.length - 1
   string_array = string.split("")
-  while index >= 0
-    reversed_string.push(string_array[index])
-    index -= 1
+  string.length.times do 
+    reversed_string << string_array.pop
   end
   reversed_string.join
 end
