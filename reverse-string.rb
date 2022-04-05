598615f1f61b111d831e595a49140d3448ffbce5
greg <gregorybwest@gmail.com>
2022-04-05 13:33:57 -0700

    whiteboarding solution to reverse-string

---
+++ reverse-string.rb
@@ -0,0 +1,23 @@
+# Write a function that takes in a string as a parameter and returns the reverse of that string
+
+# Input: "hello"
+# Output: "olleh"
+
+# Write a function that takes in a string as a parameter
+# Set a variable to an empty string which will eventually contain the reversed string
+# Split the string into an array of characters
+# Loop through each element of the array of characters starting at the end of the array, pushing each element into the reversed string variable. 
+# Return the reversed string
+
+def reverse_string(string)
+  reversed_string = []
+  index = string.length - 1
+  string_array = string.split("")
+  while index >= 0
+    reversed_string.push(string_array[index])
+    index -= 1
+  end
+  reversed_string.join
+end
+
+p reverse_string("how are you")
