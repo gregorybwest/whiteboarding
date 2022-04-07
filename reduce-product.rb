4582dc8311cea14134666f197963b8a8c0305199
greg <gregorybwest@gmail.com>
2022-04-06 19:27:40 -0700

    example inputs/outputs and pseudocode for reduce-product

---
+++ reduce-product.rb
@@ -0,0 +1,17 @@
+# Write a function that accepts an array of numbers and returns the product of all the numbers.
+
+# Input: [1, 2, 3, 4]
+# Output: 24
+
+# Input: [1, 0, 3, 4]
+# Output: 0
+
+# Input: [-1, 2, 3, 4]
+# Output: -24
+
+# Write a function that accepts an array of integers as an argument
+# Set a variable that will contain the product of the numbers in the array to the first number in the array
+# Loop through the array starting at array[1], muliplying each element by the variable
+  # increment index as long as it's less than the length of the array
+# Return the variable storing the product of all the numbers in the array
+
