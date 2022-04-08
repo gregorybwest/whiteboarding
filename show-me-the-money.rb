7cf264b549cc360478c8c046dc3ebdcc66ebadfa
greg <gregorybwest@gmail.com>
2022-04-08 12:09:16 -0700

    two more solutions using while loop and each loop

--- show-me-the-money.rb
+++ show-me-the-money.rb
@@ -6,11 +6,11 @@
 # Input: “abcdefghijklmnopqrstuvwxyz”
 # Output: false
 
-def show_me_the_money(string)
-  string.include?("$")
-end
+# def show_me_the_money(string)
+#   string.include?("$")
+# end
 
-p show_me_the_money("abcdefghijklmnopqrstuvwxyz")
+# p show_me_the_money("abcdefghijklmnopqrstuvwxyz")
 
 # Write a function that takes in a string as an argument
 # Set index to 0
@@ -18,3 +18,26 @@ p show_me_the_money("abcdefghijklmnopqrstuvwxyz")
   # If the character at current index is a "$" symbol, return true and break
   # Else if we get to the end of the string and don't find the "$" symbol, return false
 
+# def show_me_the_money(string)
+#   index = 0
+#   while index < string.length
+#     if string[index] == "$"
+#       return true
+#     end
+#     index += 1
+#   end
+#   return false
+# end
+
+def show_me_the_money(string)
+  string.chars.each do |letter|
+    if letter == "$"
+      return true
+    end
+  end
+  return false
+end
+
+p show_me_the_money("abcdefghi$jklmnopqrstuvwxyz")  # ==> returns true
+
+p show_me_the_money("abcdefghijklmnopqrstuvwxyz")  # ==> returns false
