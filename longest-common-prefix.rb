df2b0088c4bf3ecef339357771f3b2aea3efe477
greg <gregorybwest@gmail.com>
2022-04-07 17:16:08 -0700

    pseudocode and solution to longest-common-prefix

---
+++ longest-common-prefix.rb
@@ -0,0 +1,31 @@
+# Please write a function that, given two strings, returns
+# the longest common prefix between the strings.
+# 
+# For example:
+# CAT, CAR => "CA"
+# LOG, LOCK => "LO"
+# BABY, PARTY => ""
+# CAT, CAT => "CAT"
+# CAT, CATER => "CAT"
+# 123, None => raise exception
+
+# Write a function that takes in two strings as arguments
+# Set a variable to an empty string that will store the letter of the common prefix of two strings
+# Set index to 0
+# While index is less than the length of the shorter string
+  # If the letter at the current index for string 1 is equal to the letter at the current index of string 2, add that letter to common_prefix string
+  # Else, break
+# Return common_prefix string
+
+
+def longest_common_prefix(string1, string2)
+  common_prefix = ""
+  index = 0
+  while string1[index] == string2[index]
+    common_prefix += string1[index]
+    index += 1
+  end
+  common_prefix
+end
+
+p longest_common_prefix("CAT", "CAR")
