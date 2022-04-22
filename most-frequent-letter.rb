7f57ce0b13c7e18d103ed812f404aa6971ced2e1
greg <gregorybwest@gmail.com>
2022-04-22 10:57:02 -0700

    pseudocode and initial solution to most-frequent-letter

---
+++ most-frequent-letter.rb
@@ -0,0 +1,38 @@
+# Write a function that returns the most frequently occurring letter in a string
+
+# Input: "peter piper picked a peck of pickled peppers"
+# Output: "p"
+
+# Write a function
+# Create an empty hash which will store the frequency of each letter's occurence
+# Set a variable to 0 that stores most frequent letter count
+# Set a variable to empty string that stores most frequent letter
+# Loop through the string
+  # If the letter is in the hash, increment the value of the key by 1
+  # If the letter is not in the hash, add it to the hash with a value of 1
+  # If the letter count is higher than the most frequent count
+    # Set most frequent count to letter count
+    # Set most frequent letter to letter
+# Return frequent letter
+
+def frequent_letter(string)
+  letter_count = {}
+  index = 0
+  most_frequent_count = 0
+  most_frequent_letter = ""
+  while index < string.length
+    if letter_count[string[index]]
+      letter_count[string[index]] += 1
+    else
+      letter_count[string[index]] = 1
+    end
+    if letter_count[string[index]] > most_frequent_count
+      most_frequent_count = letter_count[string[index]]
+      most_frequent_letter = string[index]
+    end
+    index += 1
+  end
+  most_frequent_letter
+end
+
+p frequent_letter("peter piper picked a peck of pickled peppers") # ==> "p"
