1518c9b41de210b23732c319c87133531839e05d
greg <gregorybwest@gmail.com>
2022-05-11 12:33:21 -0700

    decomposed solution to balloon-count problem

--- balloon-count.rb
+++ balloon-count.rb
@@ -17,24 +17,31 @@
 # divide "o" and "l" values by 2
 # return the minimum value from the hash
 
+# def balloon_counter(string)
+#   balloon_hash = {
+#     "b" => 0,
+#     "a" => 0,
+#     "l" => 0, 
+#     "o" => 0,
+#     "n" => 0 
+#   }
+#   string.each_char do |char|
+#     if balloon_hash[char]
+#       balloon_hash[char] += 1
+#     end
+#   end
+#   return balloon_hash.values.min
+# end
+
+
+
+
 def balloon_counter(string)
-  balloon_hash = {
-    "b" => 0,
-    "a" => 0,
-    "l" => 0, 
-    "o" => 0,
-    "n" => 0 
-  }
-  string.each_char do |char|
-    if balloon_hash[char]
-      balloon_hash[char] += 1
-    end
-  end
-  return balloon_hash.values.min
+  [string.count("b"), string.count("a"), string.count("l"), string.count("o"), string.count("n")].min
 end
 
-p balloon_counter("bxaxlxlocovn")
-p balloon_counter("bxaxlxlocovnbxaxlxlocovn")
-p balloon_counter("hello")
-
-
+p balloon_counter("bxaxlxlocovn") == 1
+p balloon_counter("nlaebolko") == 1
+p balloon_counter("bxaxlxlocovnbxaxlxlocovn") == 2
+p balloon_counter("loonbalxballpoon") == 2
+p balloon_counter("hello") == 0
