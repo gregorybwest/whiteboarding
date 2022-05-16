09333245859aeb68918303c45b4da6af99682ae2
greg <gregorybwest@gmail.com>
2022-05-16 15:22:13 -0700

    adds #9 solution

--- data-transformation-practice.rb
+++ data-transformation-practice.rb
@@ -112,7 +112,23 @@ while index < items.length
 end
 p combined_hash
 
+# 9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
+# For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}
 
+books = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}]
+books_hash = {}
+index = 0
+while index < books.length
+  book = books[index]
+  author = book[:author]
+  title = book[:title]
+  if books_hash[author] == nil
+    books_hash[author] = []
+  end
+  books_hash[author] << title
+  index += 1
+end
+p books_hash
 
 
 
