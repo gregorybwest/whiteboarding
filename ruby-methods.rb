9600861059a010aac377bc900eabc6a32ead915c
greg <gregorybwest@gmail.com>
2022-05-31 21:15:06 -0700

    adds 'shift' method example

--- ruby-methods.rb
+++ ruby-methods.rb
@@ -3,3 +3,10 @@
 array = [18, 22, 33, 3, 5, 6]
 p array.select {|num| num >= 6 }
 p array.select {|num| num.odd? }
+
+# shift method
+
+array = [18, 22, 33, 3, 5, 6]
+p array
+p array.shift # ==> returns 18
+p array # ==> [22, 33, 3, 5, 6]
