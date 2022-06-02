faf7dd9be390840406ee96a0af5e930138bb71aa
greg <gregorybwest@gmail.com>
2022-06-01 17:39:13 -0700

    adds 'unshift' meethod example

--- ruby-methods.rb
+++ ruby-methods.rb
@@ -10,3 +10,10 @@ array = [18, 22, 33, 3, 5, 6]
 p array
 p array.shift # ==> returns 18
 p array # ==> [22, 33, 3, 5, 6]
+
+
+array = [18, 3, 100]
+p array
+p array.unshift(17) # ==> returns [17, 18, 3, 100]
+p array # ==> [17, 18, 3, 100]
+
