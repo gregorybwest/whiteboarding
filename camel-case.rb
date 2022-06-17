37145c41f90323a6071d64afbc691b7783bfcb35
greg <gregorybwest@gmail.com>
2022-06-17 13:34:37 -0700

    adds error handling for non-camelCase words passed as argument

--- camel-case.rb
+++ camel-case.rb
@@ -1,6 +1,9 @@
 # this function takes in a string, s, and returns the number of words contained in the string, given that the string is written in camelcase. 
 
 def camel_case(s)
+  if s[0] != s[0].downcase
+    raise ArgumentError.new("camelCase strings must begin with a lowercase letter")
+  end
   counter = 0
   s.each_char do |letter|
     if letter == letter.upcase
@@ -12,3 +15,5 @@ end
 
 p camel_case("helloThere") == 2
 p camel_case("thisIsAMethodName") == 5
+p camel_case("NotCamelCase") # this will raise ArgumentError
+
