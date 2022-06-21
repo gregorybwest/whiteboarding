e34d945912fcb492ef48f78694b7b93a5fd5b78d
greg <gregorybwest@gmail.com>
2022-06-20 17:51:24 -0700

    solves problem 1 with each loop

--- data-transformation-practice.rb
+++ data-transformation-practice.rb
@@ -234,6 +234,15 @@ end
 p array
 
 
+#  1. Convert an array of arrays into a hash.
+#  For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+array = [[1, 3], [8, 9], [2, 16]]
+hash = {}
+array.each do |pair|
+  hash[pair[0]] = pair[1]
+end
+p hash
 
 
 
