275d55a929e0c7097026620257927e65fd7b6036
greg <gregorybwest@gmail.com>
2022-07-10 17:31:30 -0700

    has-duplicate-value algorithm solution with O(n2) efficiency

---
+++ has-duplicate-value.rb
@@ -0,0 +1,25 @@
+# write a function that returns true if the given array contains at least one duplicate value, and false if it contains no duplicate values
+
+# input: [3, 5, 9, 4]
+# output: false
+
+# input: [3, 5, 7, 9, 3]
+# output: true
+
+def has_duplicate_value?(array)
+  i = 0
+  while i < array.length
+    j = 0
+    while j < array.length
+      if i != j && array[i] == array[j]
+        return true
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return false
+end
+
+p has_duplicate_value?([3, 5, 9, 4]) == false
+p has_duplicate_value?([3, 5, 7, 9, 3]) == true
