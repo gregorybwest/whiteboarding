613362642d8aba9cacf43b6d84d0777e5e1520f3
greg <gregorybwest@gmail.com>
2022-07-10 17:40:41 -0700

    O(n) solution using hash

--- has-duplicate-value.rb
+++ has-duplicate-value.rb
@@ -6,17 +6,32 @@
 # input: [3, 5, 7, 9, 3]
 # output: true
 
+# def has_duplicate_value?(array)
+#   i = 0
+#   while i < array.length
+#     j = 0
+#     while j < array.length
+#       if i != j && array[i] == array[j]
+#         return true
+#       end
+#       j += 1
+#     end
+#     i += 1
+#   end
+#   return false
+# end
+
+# p has_duplicate_value?([3, 5, 9, 4]) == false
+# p has_duplicate_value?([3, 5, 7, 9, 3]) == true
+
 def has_duplicate_value?(array)
-  i = 0
-  while i < array.length
-    j = 0
-    while j < array.length
-      if i != j && array[i] == array[j]
-        return true
-      end
-      j += 1
+  number_hash = {}
+  array.each do |number|
+    if number_hash[number]
+      return true
+    else
+      number_hash[number] = 1
     end
-    i += 1
   end
   return false
 end
