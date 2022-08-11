c196f1a5b87854ed568974bcc832d1c9d88a650a
greg <gregorybwest@gmail.com>
2022-08-10 17:11:30 -0700

    alternate O(N) solution to missing letter problem

--- missing_letter.rb
+++ missing_letter.rb
@@ -5,21 +5,39 @@
 # Input: “The quick brown box jumps over a lazy dog”
 # Output: “f”
 
+# def missing_letter(string)
+#   alphabet = {
+#     "a" => 0, "b" => 0, "c" => 0, "d" => 0, "e" => 0, "f" => 0, "g" => 0, "h" => 0, "i" => 0, "j" => 0, "k" => 0, "l" => 0, "m" => 0, "n" => 0, "o" => 0, "p" => 0, "q" => 0, "r" => 0, "s" => 0, "t" => 0, "u" => 0, "v" => 0, "w" => 0, "x" => 0, "y" => 0, "z" => 0
+#   }
+#   string.each_char do |letter|
+#     if letter != " "
+#       alphabet[letter.downcase] += 1
+#     end
+#   end
+#   alphabet.each do |key, value|
+#     if value == 0
+#       return key
+#     end
+#   end
+# end
+
+# p missing_letter("The quick brown box jumps over a lazy dog")
+# p missing_letter("The QUICK brown box jumps OVER a lazy DOG")
+
 def missing_letter(string)
-  alphabet = {
-    "a" => 0, "b" => 0, "c" => 0, "d" => 0, "e" => 0, "f" => 0, "g" => 0, "h" => 0, "i" => 0, "j" => 0, "k" => 0, "l" => 0, "m" => 0, "n" => 0, "o" => 0, "p" => 0, "q" => 0, "r" => 0, "s" => 0, "t" => 0, "u" => 0, "v" => 0, "w" => 0, "x" => 0, "y" => 0, "z" => 0
-  }
+  # store all encountered letter in a hash table
+  letter_hash = {}
   string.each_char do |letter|
-    if letter != " "
-      alphabet[letter.downcase] += 1
-    end
+    letter_hash[letter] = true
   end
-  alphabet.each do |key, value|
-    if value == 0
-      return key
+  # return first unseen letter
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  alphabet.each_char do |letter|
+    if !letter_hash[letter]
+      return letter
     end
   end
 end
 
-p missing_letter("The quick brown box jumps over a lazy dog")
-p missing_letter("The QUICK brown box jumps OVER a lazy DOG")
+p missing_letter("The quick brown box jumps over a lazy dog") == "f" # true
+
