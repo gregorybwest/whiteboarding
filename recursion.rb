f1d77725d2b2cd4fd3130452846292b168023670
greg <gregorybwest@gmail.com>
2022-08-13 17:20:36 -0700

    print range using recursion

--- recursion.rb
+++ recursion.rb
@@ -1,10 +1,18 @@
-def countdown(num)
-  while num >= 0 
-    puts num
-    num -= 1
+# def countdown(num)
+#   while num >= 0 
+#     puts num
+#     num -= 1
+#   end
+# end
+
+# countdown(10)
+
+def sum(low, high)
+  if low > high
+    return low
   end
+  return high + sum(low, high - 1)
 end
 
-countdown(10)
-
+p sum(0, 10)
 
