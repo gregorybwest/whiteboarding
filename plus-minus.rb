e7190560bb68f84d42e55312597d072251d3b209
greg <gregorybwest@gmail.com>
2022-10-03 16:01:16 -0700

    solution to hacker rank plus-minus, doesn't specify number of decimals after quotient

---
+++ plus-minus.rb
@@ -0,0 +1,25 @@
+# Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+def plus_minus(arr)
+  positive_nums = 0
+  zero_nums = 0
+  negative_nums = 0
+  arr.each do |num|
+    if num > 0
+      positive_nums += 1
+    elsif num == 0
+      zero_nums += 1
+    else
+      negative_nums += 1
+    end
+  end
+  puts (positive_nums.to_f / arr.length)
+  puts (negative_nums.to_f / arr.length)
+  puts (zero_nums.to_f / arr.length)
+ 
+
+  
+end
+
+plus_minus([1, 1, 0, -1, -1])
+plus_minus([-4, 3, -9, 0, 4, 1])
