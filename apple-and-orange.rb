b6207af214852adb8a675faebb40c71f25a6e576
greg <gregorybwest@gmail.com>
2022-10-05 18:06:44 -0700

    hackerrank apple-and-orange, pseudocode and solution works for simple test case but times out

---
+++ apple-and-orange.rb
@@ -0,0 +1,44 @@
+# Function Description
+
+# Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+# countApplesAndOranges has the following parameter(s):
+
+# s: integer, starting point of Sam's house location.
+# t: integer, ending location of Sam's house location.
+# a: integer, location of the Apple tree.
+# b: integer, location of the Orange tree.
+# apples: integer array, distances at which each apple falls from the tree.
+# oranges: integer array, distances at which each orange falls from the tree.
+
+# create empty apple location array that will store location of apple after falling
+# create empty orange location array that will store location of orange after falling
+# create counter for apples that fall on house, set to 0
+# create counter for oranges that fall on house, set to 0
+# loop through apples, adding location "a" to each distance fallen, add result to apple location array
+# loop through oranges, adding location "b" to each distance fallen, add result to orange location array
+
+
+
+
+def count_apples_and_oranges(s, t, a, b, apples, oranges)
+  apple_locations = []
+  orange_locations = []
+  apple_counter = 0
+  orange_counter = 0
+  apples.each { |apple| apple_locations << a + apple }
+  oranges.each { |orange| orange_locations << b + orange }
+  (s..t).each do |num|
+    if apple_locations.include?(num)
+      apple_counter += 1
+    end
+    if orange_locations.include?(num)
+      orange_counter += 1
+    end
+  end
+  p apple_counter
+  p orange_counter
+end
+
+count_apples_and_oranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
+
