fd5e7ea8ce8c18d58b989d65f7742e12a427dffa
greg <gregorybwest@gmail.com>
2022-11-01 19:33:02 -0700

    has-letters function in ruby, problem from saleforce job interview

---
+++ has-letters.rb
@@ -0,0 +1,30 @@
+# // Write a function named hasLetters() that receives a string and an Array of letters and returns TRUE if the string contains all of the letters in the array
+
+def has_letters(string, array)
+  letter_hash = {}
+  string.each_char do |letter|
+    if letter_hash[letter] 
+      letter_hash[letter] += 1
+    else
+      letter_hash[letter] = 1
+    end
+  end
+  p letter_hash
+  array.each do |letter|
+    if letter_hash[letter]
+      letter_hash[letter] -= 1
+    end
+  end
+  p letter_hash
+  letter_hash.each do |key, value|
+    p value
+    if value != 0 
+      return false
+    end
+  end
+  return true
+
+end
+
+
+p has_letters("hello", ["h", "e", "l", "l", "o"])
