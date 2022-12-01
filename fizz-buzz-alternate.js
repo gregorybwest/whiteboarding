1d5662ee4b58514959f5037f5cc21bfc2c1799a7
greg <gregorybwest@gmail.com>
2022-11-30 16:51:05 -0800

    fizz-buzz type alogorithm in javascript

---
+++ fizz-buzz-alternate.js
@@ -0,0 +1,15 @@
+// write a program that prints all the numbers between 2000 and 3200 inclusive that are divisible by 7 but not divisible by 5. Each number should be separted by a comma and space
+
+function returnNums() {
+  let solution = [];
+  for (let num = 2000; num <= 3200; num++) {
+    if (num % 7 === 0) {
+      if (num % 5 !== 0) {
+        solution.push(num);
+      }
+    }
+  }
+  return solution.join(", ");
+}
+
+console.log(returnNums());
