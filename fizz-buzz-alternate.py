2b529a785b7821494f5a6bb340f780d63ee0fd8d
greg <gregorybwest@gmail.com>
2022-12-07 11:10:07 -0800

    append nums to empty list, and join them with ', '

--- fizz-buzz-alternate.py
+++ fizz-buzz-alternate.py
@@ -1,7 +1,9 @@
 # write a program that prints all the numbers between 2000 and 3200 inclusive that are divisible by 7 but not divisible by 5. Each number should be separted by a comma and space
 
+nums = []
 i = 2000
 while i <= 3200:
     if (i % 7 == 0) & (i % 5 != 0):
-        print(i)
+        nums.append(i)
     i += 1
+print(", ".join(str(num) for num in nums))
