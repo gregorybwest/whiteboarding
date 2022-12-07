4d45d4c3e10d8ca6d5cdf8148db46a7614adf610
greg <gregorybwest@gmail.com>
2022-12-06 18:04:18 -0800

    python fizz-buzz-alternate solution

---
+++ fizz-buzz-alternate.py
@@ -0,0 +1,7 @@
+# write a program that prints all the numbers between 2000 and 3200 inclusive that are divisible by 7 but not divisible by 5. Each number should be separted by a comma and space
+
+i = 2000
+while i <= 3200:
+    if (i % 7 == 0) & (i % 5 != 0):
+        print(i)
+    i += 1
