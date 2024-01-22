d29bbdfcd767be4c119060c8a53c33042e586382
greg <gregorybwest@gmail.com>
2024-01-22 14:42:00 -0800

    conditional handles edge case of double digit hours

--- work-hours.rb
+++ work-hours.rb
@@ -7,7 +7,12 @@ def work_hours(work_log)
   index = 0
   while index < work_log.length
     if work_log[index] == "h" && work_log[index + 1] == "o" && work_log[index + 2] == "u" && work_log[index + 3] == "r" && work_log[index + 4] == "s"
-      hours << work_log[index - 2].to_i
+      if work_log[index - 3].to_i > 0
+        hours << (work_log[index - 3] + work_log[index - 3]).to_i
+      else
+        hours << work_log[index - 2].to_i
+      end
+      
     end
     index += 1
   end
