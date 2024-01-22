ffb784bee4f01244c1a37a543bdbe3b6d0709725
greg <gregorybwest@gmail.com>
2024-01-22 10:40:01 -0800

    algorithm that takes in entire work log as argument and returns sum of total hours worked for given period

---
+++ work-hours.rb
@@ -0,0 +1,17 @@
+# iterate through each char of a string
+  # if you find a number that is followed by the word "hours", store that number in an array
+# find the sum of the array
+
+def work_hours(work_log)
+  hours = []
+  index = 0
+  while index < work_log.length
+    if work_log[index] == "h" && work_log[index + 1] == "o" && work_log[index + 2] == "u" && work_log[index + 3] == "r" && work_log[index + 4] == "s"
+      hours << work_log[index - 2].to_i
+    end
+    index += 1
+  end
+  hours.sum
+end
+
+puts work_hours("")
